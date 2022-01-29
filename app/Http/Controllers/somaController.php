e334e7caf4db8519ea757aafc0a7f1d06edb80c6
eduardo.costa07 <eduardo.costa@mazzafc.tech>
2022-01-28 21:23:34 -0300

    teste

---
+++ app/Http/Controllers/somaController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class somaController extends Controller
+{
+    public function soma($valor1,$valor2){
+        $soma = $valor1 + $valor2;
+        logger()->info('Soma feita');
+       return $soma;
+    }
+    public function sub($num1,$num2){
+        $sub = $num1 - $num2;
+        logger()->info('Sub feita',['num1' => $num1, 'num2' => $num2, 'sub' =>$sub ]);
+        return $sub;
+    }
+    public function division($num1,$num2)
+    {
+        if ($num2 == 0) {
+            logger()->error('Divisor zero!');
+            return false;
+        }
+        $div = $num1/$num2;
+        return $div;
+    }
+        public function negativos($num1,$num2){
+
+        if($num1 < 0 || $num2 < 0  ){
+            logger()->warning('Negativo!');
+        }
+    }
+}
